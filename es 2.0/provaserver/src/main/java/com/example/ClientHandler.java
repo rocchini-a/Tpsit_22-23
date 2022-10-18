832506b23f899fa403670124e231386521278740
rocchini-a <98748614+rocchini-a@users.noreply.github.com>
2022-10-18 18:34:00 +0200

    Add files via upload

---
+++ es 2.0/provaserver/src/main/java/com/example/ClientHandler.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientHandler extends Thread {
+    private Socket s;
+    private PrintWriter pr = null;
+    private BufferedReader br = null;
+    static int c =0;
+    public ClientHandler(Socket s) {
+        this.s = s;
+        try {
+            // per parlare
+            pr = new PrintWriter(s.getOutputStream(), true);
+            // per ascoltare
+            br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+ 
+        c++;
+        try {
+            System.out.println(br.readLine());
+            pr.println("Ciao come ti chiami?"); // invio messaggio
+            String nome = br.readLine(); // ricevo: il peso
+            System.out.println(nome);
+            pr.println("Benvenuto " + nome + "sei l'utente " +c ); // invio messaggio
+           
+         
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
